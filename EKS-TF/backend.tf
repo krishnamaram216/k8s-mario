b9a2582c999850353f5c363a20203bb9a8326696
krishnamaram216 <163405191+krishnamaram216@users.noreply.github.com>
2024-09-26 09:35:43 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "projectmario" # Replace with your actual S3 bucket name
+    bucket = "projectmariomy" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
